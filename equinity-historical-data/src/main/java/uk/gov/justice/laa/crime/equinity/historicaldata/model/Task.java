3b5d1aa557c5a28a81b0fc575874149dd8c46e61
Tomas <tomas.novellino@digital.justice.gov.uk>
2024-01-25 15:25:49 +0000

    removed unused import

--- equinity-historical-data/src/main/java/uk/gov/justice/laa/crime/equinity/historicaldata/model/Task.java
+++ equinity-historical-data/src/main/java/uk/gov/justice/laa/crime/equinity/historicaldata/model/Task.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.Columns;
 
 import java.time.LocalDateTime;
 
