6cc5c38c445e68b5f40d925d527ba9ca73b01342
Tomas <tomas.novellino@digital.justice.gov.uk>
2024-03-22 10:58:13 +0000

    EMP-181: Updated libraries and securitu settings

---
+++ equinity-historical-data/src/main/java/uk/gov/justice/laa/crime/equinity/historicaldata/config/EquinitiAuthenticationConfig.java
@@ -0,0 +1,31 @@
+package uk.gov.justice.laa.crime.equinity.historicaldata.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class EquinitiAuthenticationConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(
+                authorizeHttpRequests -> authorizeHttpRequests
+                    .requestMatchers("/token/**").permitAll()
+                    .requestMatchers("/swagger-ui/**").permitAll()
+                    .requestMatchers("/api-docs/**").permitAll()
+                    .requestMatchers("/actuator/**").permitAll()
+                    .anyRequest().permitAll()
+            )
+            .sessionManagement(
+                session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            );
+//            .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+}
