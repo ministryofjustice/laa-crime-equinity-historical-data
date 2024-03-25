fba3a389d0586e840a5d7b61f78d962ed4b5ee6b
Tomas <tomas.novellino@digital.justice.gov.uk>
2024-03-25 13:19:47 +0000

    EMP-181: Added classes to check Authentication based on API-KEY
    EMP-181: Added classes to check Authentication based on API-KEY

---
+++ equinity-historical-data/src/main/java/uk/gov/justice/laa/crime/equinity/historicaldata/security/ApiClientAuthFilter.java
@@ -0,0 +1,26 @@
+package uk.gov.justice.laa.crime.equinity.historicaldata.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class ApiClientAuthFilter extends OncePerRequestFilter {
+    private final ApiClientAuthExtractor extractor;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        extractor.extract(request)
+                .ifPresent(SecurityContextHolder.getContext()::setAuthentication);
+
+        filterChain.doFilter(request, response);
+    }
+}
---
+++ equinity-historical-data/src/main/java/uk/gov/justice/laa/crime/equinity/historicaldata/security/SecurityConfiguration.java
@@ -0,0 +1,41 @@
+package uk.gov.justice.laa.crime.equinity.historicaldata.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+    @Value("${server.api.url-path}")
+    private String API_REQUEST_PATH;
+    private final ApiClientAuthFilter authFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+            .cors(AbstractHttpConfigurer::disable)
+            .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .formLogin(AbstractHttpConfigurer::disable)
+            .securityMatcher("/**")
+            .authorizeHttpRequests(registry -> registry
+                .requestMatchers("/token/**").permitAll()
+                .requestMatchers("/swagger-ui/**").permitAll()
+                .requestMatchers("/api-docs/**").permitAll()
+                .requestMatchers("/actuator/**").permitAll()
+                .requestMatchers(String.format("%s/**", API_REQUEST_PATH)).authenticated()
+                .anyRequest().denyAll()
+            )
+            .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
+            .build();
+    }
+}
