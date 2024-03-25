b28757fccc1874d1ceb6789f59010e132a51c2d2
Tomas <tomas.novellino@digital.justice.gov.uk>
2024-03-25 22:29:38 +0000

    EMP-181: Fixing files rename

--- equinity-historical-data/src/main/java/uk/gov/justice/laa/crime/equinity/historicaldata/archive/repository/CrmFormsArchiveRepository.java
+++ equinity-historical-data/src/main/java/uk/gov/justice/laa/crime/equinity/historicaldata/archive/repository/CrmFormsArchiveRepository.java
@@ -3,8 +3,8 @@ package uk.gov.justice.laa.crime.equinity.historicaldata.archive.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import uk.gov.justice.laa.crime.equinity.historicaldata.archive.model.CrmFormArchiveModel;
-import uk.gov.justice.laa.crime.equinity.historicaldata.config.CrmFormsModelInterface;
+import uk.gov.justice.laa.crime.equinity.historicaldata.config.CrmFormModelInterface;
 
 
-public interface CrmFormsArchiveRepository extends JpaRepository<CrmFormArchiveModel, Integer>, JpaSpecificationExecutor<CrmFormsModelInterface> {
+public interface CrmFormsArchiveRepository extends JpaRepository<CrmFormArchiveModel, Integer>, JpaSpecificationExecutor<CrmFormModelInterface> {
 }
--- equinity-historical-data/src/main/java/uk/gov/justice/laa/crime/equinity/historicaldata/config/CrmFormSearchCriteria.java
+++ equinity-historical-data/src/main/java/uk/gov/justice/laa/crime/equinity/historicaldata/config/CrmFormSearchCriteria.java
@@ -10,7 +10,7 @@ import java.time.LocalDate;
 @Configuration
 @NoArgsConstructor
 public class CrmFormSearchCriteria {
-    public Specification<CrmFormsModelInterface> getSpecification(CrmFormSearchCriteriaDTO crmFormSearchCriteriaDTO) {
+    public Specification<CrmFormModelInterface> getSpecification(CrmFormSearchCriteriaDTO crmFormSearchCriteriaDTO) {
         return Specification
             .where(byUsn(crmFormSearchCriteriaDTO.usn())
             .and(byClientName(crmFormSearchCriteriaDTO.client()))
@@ -20,31 +20,31 @@ public class CrmFormSearchCriteria {
             .and(byProviderAccount(crmFormSearchCriteriaDTO.providerAccount())));
     }
 
-    private Specification<CrmFormsModelInterface> byUsn(@Nullable String usn){
+    private Specification<CrmFormModelInterface> byUsn(@Nullable String usn){
         return (root, query, criteriaBuilder)
                 -> usn == null ? null : criteriaBuilder.like(root.get("USN"), String.format("%%%s%%", usn));
     }
 
-    private Specification<CrmFormsModelInterface> byClientName(@Nullable String clientName){
+    private Specification<CrmFormModelInterface> byClientName(@Nullable String clientName){
         return (root, query, criteriaBuilder)
                 -> clientName == null ? null : criteriaBuilder.like(root.get("clientName"), String.format("%%%s%%", clientName));
     }
 
-    private Specification<CrmFormsModelInterface> byClientDoB(@Nullable String clientDOB){
+    private Specification<CrmFormModelInterface> byClientDoB(@Nullable String clientDOB){
         return null;
     }
 
-    private Specification<CrmFormsModelInterface> byDateSubmittedFrom(@Nullable LocalDate dateSubmittedFrom){
+    private Specification<CrmFormModelInterface> byDateSubmittedFrom(@Nullable LocalDate dateSubmittedFrom){
         return (root, query, criteriaBuilder)
                 -> dateSubmittedFrom == null ? null : criteriaBuilder.greaterThanOrEqualTo(root.get("submittedDate"), dateSubmittedFrom.toString());
     }
 
-    private Specification<CrmFormsModelInterface> byDateSubmittedTo(@Nullable LocalDate dateSubmittedTo){
+    private Specification<CrmFormModelInterface> byDateSubmittedTo(@Nullable LocalDate dateSubmittedTo){
         return (root, query, criteriaBuilder)
                 -> dateSubmittedTo == null ? null : criteriaBuilder.lessThanOrEqualTo(root.get("submittedDate"), dateSubmittedTo.toString());
     }
 
-    private Specification<CrmFormsModelInterface> byProviderAccount(@Nullable String providerAccount){
+    private Specification<CrmFormModelInterface> byProviderAccount(@Nullable String providerAccount){
         return (root, query, criteriaBuilder)
             -> providerAccount == null ? null : criteriaBuilder.like(root.get("providerAccount"), String.format("%%%s%%", providerAccount));
     }
