f6c821e9ea5c5d01466ca15581d4c9ff43263b3c
Tomas <tomas.novellino@digital.justice.gov.uk>
2024-05-03 14:04:51 +0100

    EMP-210: Adding test coverage and controller skeleton for CRM7

--- equinity-historical-data/src/main/java/uk/gov/justice/laa/crime/equinity/historicaldata/controller/Crm7Controller.java
+++ equinity-historical-data/src/main/java/uk/gov/justice/laa/crime/equinity/historicaldata/controller/Crm7Controller.java
@@ -2,11 +2,12 @@ package uk.gov.justice.laa.crime.equinity.historicaldata.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.justice.laa.crime.equinity.historicaldata.generated.api.Crm7InterfaceApi;
 import uk.gov.justice.laa.crime.equinity.historicaldata.generated.dto.Crm7DetailsDTO;
+import uk.gov.justice.laa.crime.equinity.historicaldata.model.Crm7DetailsModel;
+import uk.gov.justice.laa.crime.equinity.historicaldata.model.Crm7Model;
 import uk.gov.justice.laa.crime.equinity.historicaldata.service.CrmFileService;
 
 @RestController
@@ -18,9 +19,7 @@ public class Crm7Controller implements Crm7InterfaceApi {
     @Override
     public ResponseEntity<Crm7DetailsDTO> getApplicationCrm7(Long usn) {
         log.info("eForm CRM7 details request received :: usn=[{}]", usn);
-        JSONObject crm7File = crmFileService.getCrmFormJson(usn);
-//        Crm7DetailsDTO crm7DetailsDTO = crmFileService.getCrmFileData(usn);
-        Crm7DetailsDTO crm7DetailsDTO = null;
-        return ResponseEntity.ok(crm7DetailsDTO);
+        Crm7DetailsModel crm7FileDetails = crmFileService.getCrmFileContent(usn, Crm7Model.class).getFormDetails();
+        return ResponseEntity.ok(null);
     }
 }
--- equinity-historical-data/src/test/java/uk/gov/justice/laa/crime/equinity/historicaldata/controller/Crm7ControllerTest.java
+++ equinity-historical-data/src/test/java/uk/gov/justice/laa/crime/equinity/historicaldata/controller/Crm7ControllerTest.java
@@ -1,14 +1,28 @@
 package uk.gov.justice.laa.crime.equinity.historicaldata.controller;
 
+import io.micrometer.core.instrument.util.IOUtils;
 import org.assertj.core.api.SoftAssertions;
 import org.assertj.core.api.junit.jupiter.InjectSoftAssertions;
 import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
+import org.json.JSONObject;
+import org.json.XML;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.InvalidDataAccessApiUsageException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import uk.gov.justice.laa.crime.equinity.historicaldata.exception.ResourceNotFoundException;
+import uk.gov.justice.laa.crime.equinity.historicaldata.generated.dto.Crm7DetailsDTO;
+import uk.gov.justice.laa.crime.equinity.historicaldata.model.TaskImageFilesModel;
+import uk.gov.justice.laa.crime.equinity.historicaldata.repository.TaskImageFilesRepository;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @SpringBootTest
 @ExtendWith(SoftAssertionsExtension.class)
@@ -18,18 +32,50 @@ class Crm7ControllerTest {
     private SoftAssertions softly;
 
     @Autowired
+    TaskImageFilesRepository taskImageFilesRepository;
+
+    @Autowired
     Crm7Controller controller;
+
+    @BeforeAll
+    void preTest() throws IOException {
+        // Mocking good XML
+        FileInputStream fis = new FileInputStream("src/test/resources/Crm5MockOFDFile.txt");
+        JSONObject mockedCrm5Json = new JSONObject(IOUtils.toString(fis, StandardCharsets.UTF_8));
+        byte[] fileDataByte = XML.toString(mockedCrm5Json).getBytes(StandardCharsets.UTF_8);
+        TaskImageFilesModel taskModel = new TaskImageFilesModel();
+        taskModel.setID(5001604L);
+        taskModel.setCrmFile(fileDataByte);
+        taskImageFilesRepository.save(taskModel);
+    }
 
     /**
      * USN input checks
      */
     @Test
-    void doSearchByTest_WhenUsnInputIsGivenNullThenReturnInvalidDataAccessApiUsageException() {
+    void getApplicationCrm7Test_WhenUsnInputIsGivenNullThenReturnInvalidDataAccessApiUsageException() {
         String expectedMessage = "not be null";
 
         // execute
         softly.assertThatThrownBy(() -> controller.getApplicationCrm7(null))
-                .isInstanceOf(InvalidDataAccessApiUsageException.class)
-                .hasMessageContaining(expectedMessage);
+            .isInstanceOf(InvalidDataAccessApiUsageException.class)
+            .hasMessageContaining(expectedMessage);
+    }
+
+    @Test
+    void getApplicationCrm7Test_WhenGivenNonExistingUsnThenReturnTaskNotFoundException() {
+        Long usnTest = 10L;
+        softly.assertThatThrownBy(() -> controller.getApplicationCrm7(usnTest))
+            .isInstanceOf(ResourceNotFoundException.class)
+            .hasMessageContaining("Task with USN").hasMessageContaining("not found");
+    }
+
+    @Test
+    void getApplicationCrm7Test_WhenGivenExistingUsnThenReturnValidResponse() {
+        Long usnTest = 5001604L;
+        ResponseEntity<Crm7DetailsDTO> result = controller.getApplicationCrm7(usnTest);
+        // TODO (EMP-217): update test assertions when search is implemented
+        softly.assertThat(result.getBody()).isNull();
+        softly.assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 }
