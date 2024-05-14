588fa96998708bdc8ce53b5cc77ae481d5716d33
solomonmanik <solomon.manikyam@digital.justice.gov.uk>
2024-05-14 09:44:16 +0100

    EMP-255 CRM4 api model classes

---
+++ equinity-historical-data/src/main/java/uk/gov/justice/laa/crime/equinity/historicaldata/model/Crm4AdditionalExpenditures.java
@@ -0,0 +1,13 @@
+package uk.gov.justice.laa.crime.equinity.historicaldata.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+@Data
+@NoArgsConstructor
+
+public class Crm4AdditionalExpenditures implements CrmFileDetailsModelInterface {    
+
+     public ArrayList<Crm4Expenditure> row;
+}
