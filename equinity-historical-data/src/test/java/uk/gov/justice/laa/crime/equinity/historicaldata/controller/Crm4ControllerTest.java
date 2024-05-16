430a12ab52e5a1a3a3c1d5bcae8f789a8e3f06c8
solomonmanik <144111359+solomonmanik@users.noreply.github.com>
2024-05-16 13:28:12 +0100

    Emp 255 (#153)
    * EMP-255 all section except Expenditure and quote
    
    * EMP-255 unit tests
    
    * EMP-255 review comments added DTO suffix to an object
    
    * EMP-255 removed middlename as it is not present in data
    
    * EMP-255 removed middlename

---
+++ equinity-historical-data/src/test/java/uk/gov/justice/laa/crime/equinity/historicaldata/controller/Crm4ControllerTest.java
@@ -0,0 +1,86 @@
+package uk.gov.justice.laa.crime.equinity.historicaldata.controller;
+
+import io.micrometer.core.instrument.util.IOUtils;
+import org.assertj.core.api.SoftAssertions;
+import org.assertj.core.api.junit.jupiter.InjectSoftAssertions;
+import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
+import org.json.JSONObject;
+import org.json.XML;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import uk.gov.justice.laa.crime.equinity.historicaldata.exception.ResourceNotFoundException;
+import uk.gov.justice.laa.crime.equinity.historicaldata.generated.dto.Crm4DetailsDTO;
+import uk.gov.justice.laa.crime.equinity.historicaldata.model.TaskImageFilesModel;
+import uk.gov.justice.laa.crime.equinity.historicaldata.repository.TaskImageFilesRepository;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+
+@SpringBootTest
+@ExtendWith(SoftAssertionsExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class Crm4ControllerTest {
+    @InjectSoftAssertions
+    private SoftAssertions softly;
+
+    @Autowired
+    TaskImageFilesRepository taskImageFilesRepository;
+
+    @Autowired
+    Crm4Controller controller;
+
+    @BeforeAll
+    void preTest() throws IOException {
+        // Mocking good XML
+        FileInputStream fis = new FileInputStream("src/test/resources/Crm4MockFile_5001912.txt");
+        JSONObject mockedCrm5Json = new JSONObject(IOUtils.toString(fis, StandardCharsets.UTF_8));
+        byte[] fileDataByte = XML.toString(mockedCrm5Json).getBytes(StandardCharsets.UTF_8);
+        TaskImageFilesModel taskModel = new TaskImageFilesModel();
+        taskModel.setID(5001912L);
+        taskModel.setCrmFile(fileDataByte);
+        taskImageFilesRepository.save(taskModel);
+    }
+
+    /**
+     * USN input checks
+     */
+    @Test
+    void getApplicationCrm4Test_WhenUsnInputIsGivenNullThenReturnInvalidDataAccessApiUsageException() {
+        String expectedMessage = "not be null";
+
+        // execute
+        softly.assertThatThrownBy(() -> controller.getApplicationCrm4(null))
+            .isInstanceOf(InvalidDataAccessApiUsageException.class)
+            .hasMessageContaining(expectedMessage);
+    }
+
+    @Test
+    void getApplicationCrm4Test_WhenGivenNonExistingUsnThenReturnTaskNotFoundException() {
+        Long usnTest = 10L;
+        softly.assertThatThrownBy(() -> controller.getApplicationCrm4(usnTest))
+            .isInstanceOf(ResourceNotFoundException.class)
+            .hasMessageContaining("Task with USN").hasMessageContaining("not found");
+    }
+
+    @Test
+    void getApplicationCrm4Test_WhenGivenExistingUsnThenReturnValidResponse() {
+        Long usnTest = 5001912L;
+        String urn = "GH65789";
+        ResponseEntity<Crm4DetailsDTO> result = controller.getApplicationCrm4(usnTest);
+
+        softly.assertThat(result.getBody()).isNotNull();
+        softly.assertThat(result.getBody()).isInstanceOf(Crm4DetailsDTO.class);
+        softly.assertThat(Objects.requireNonNull(result.getBody()).getCaseDetails().getFirm().getUrn()).isEqualTo(urn);
+        softly.assertThat(Objects.requireNonNull(result.getBody()).getExpenditureDetails().getDetails()).isNotNull();
+        softly.assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+}
