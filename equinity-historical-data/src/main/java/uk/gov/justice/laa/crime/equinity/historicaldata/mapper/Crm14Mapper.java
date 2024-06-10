93146f9a3099f0c63b7a1511fa75a437aadc7ea5
solomonmanik <solomon.manikyam@digital.justice.gov.uk>
2024-06-10 11:57:01 +0100

    EMP-320 latest mapping

--- equinity-historical-data/src/main/java/uk/gov/justice/laa/crime/equinity/historicaldata/mapper/Crm14Mapper.java
+++ equinity-historical-data/src/main/java/uk/gov/justice/laa/crime/equinity/historicaldata/mapper/Crm14Mapper.java
@@ -8,8 +8,18 @@ import uk.gov.justice.laa.crime.equinity.historicaldata.model.Crm14DetailsModel;
 @Mapper(componentModel = "spring")
 public interface Crm14Mapper extends CrmMapper {
 
-    @Mapping(target="legalRepresentativeUse.dateStamp.usn", source="usn")
-    @Mapping(target="legalRepresentativeUse.dateStamp.usn", source="usn")
+    @Mapping(target="legalRepresentativeUse.dateStamp.usn", source="datestamp_usn")
+    @Mapping(target="legalRepresentativeUse.dateStamp.date", source="datestamp_date")
+    @Mapping(target="legalRepresentativeUse.dateStamp.date", source="datestamp_time")
+    @Mapping(target="legalRepresentativeUse.dateStamp.clientName", source="surname")
+    @Mapping(target="legalRepresentativeUse.dateStamp.clientDateOfBirth", source="date_of_birth")
+    @Mapping(target="legalRepresentativeUse.legalRepUse.usn", source="usn")
+    @Mapping(target="legalRepresentativeUse.legalRepUse.urn", source="urn")
+    // TODO - check if this field can be Enum
+    @Mapping(target="legalRepresentativeUse.legalRepUse.applicationType", source="application_type")
+    @Mapping(target="legalRepresentativeUse.legalRepUse.meansTested", source="means_tested")
+
+    @Mapping(target="legalRepresentativeUse.legalRepUse.caseType", source="means_tested")
 
     Crm14DetailsDTO getDTOFromModel(Crm14DetailsModel model);
 }
