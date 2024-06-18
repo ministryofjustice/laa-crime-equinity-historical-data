f335b1be876fcbebb82928a65c20fc2386260ad5
Tomas <tomas.novellino@digital.justice.gov.uk>
2024-06-18 11:28:55 +0100

    EMP-346: Adding linked evidence to CRM5
    EMP-346: Adding linked evidence to CRM5. Small refactor to CRM5 schema moving out to single file the Open API schemas for CRM5

--- equinity-historical-data/src/main/java/uk/gov/justice/laa/crime/equinity/historicaldata/controller/Crm5Controller.java
+++ equinity-historical-data/src/main/java/uk/gov/justice/laa/crime/equinity/historicaldata/controller/Crm5Controller.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 import uk.gov.justice.laa.crime.equinity.historicaldata.config.CrmFormDetailsCriteriaDTO;
 import uk.gov.justice.laa.crime.equinity.historicaldata.generated.api.Crm5InterfaceApi;
 import uk.gov.justice.laa.crime.equinity.historicaldata.generated.dto.CRM5DetailsDTO;
+import uk.gov.justice.laa.crime.equinity.historicaldata.generated.dto.Crm5FormDTO;
 import uk.gov.justice.laa.crime.equinity.historicaldata.mapper.Crm5Mapper;
 import uk.gov.justice.laa.crime.equinity.historicaldata.model.Crm5DetailsModel;
+import uk.gov.justice.laa.crime.equinity.historicaldata.model.Crm5Model;
 import uk.gov.justice.laa.crime.equinity.historicaldata.service.CrmFileService;
 
 import static uk.gov.justice.laa.crime.equinity.historicaldata.service.CrmFileService.CRM_TYPE_5;
@@ -22,14 +24,13 @@ public class Crm5Controller implements Crm5InterfaceApi {
 
 
     @Override
-    public ResponseEntity<CRM5DetailsDTO> getApplication(Long usn, String profileAcceptedTypes) {
+    public ResponseEntity<Crm5FormDTO> getApplication(Long usn, String profileAcceptedTypes) {
         log.info("eForm CRM5 details request received :: usn=[{}]", usn);
         CrmFormDetailsCriteriaDTO crmFormDetailsCriteriaDTO = new CrmFormDetailsCriteriaDTO(
                 usn, CRM_TYPE_5, profileAcceptedTypes
         );
-        Crm5DetailsModel crm5FileDetails = (Crm5DetailsModel) crmFileService
-                .getCrmFormData(crmFormDetailsCriteriaDTO)
-                .getFormDetails();
-        return ResponseEntity.ok(crm5Mapper.getEntityFromModel(crm5FileDetails));
+
+        Crm5Model crmFormData = crmFileService.getCrmFormData(crmFormDetailsCriteriaDTO);
+        return ResponseEntity.ok(crm5Mapper.getDTOFromModel(crmFormData));
     }
 }
