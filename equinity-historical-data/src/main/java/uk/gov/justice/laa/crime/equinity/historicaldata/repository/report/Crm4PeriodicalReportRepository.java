1d722e30f423d33d4b7c8fd2db91da01da9c4b76
Tomas <tomas.novellino@digital.justice.gov.uk>
2024-06-28 09:48:20 +0100

    EMP-374: Removed Transactional annotation because it was redundant
    EMP-374: Removed Transactional annotation because it was redundant since it needs to be added to the caller instead

--- equinity-historical-data/src/main/java/uk/gov/justice/laa/crime/equinity/historicaldata/repository/report/Crm4PeriodicalReportRepository.java
+++ equinity-historical-data/src/main/java/uk/gov/justice/laa/crime/equinity/historicaldata/repository/report/Crm4PeriodicalReportRepository.java
@@ -1,6 +1,5 @@
 package uk.gov.justice.laa.crime.equinity.historicaldata.repository.report;
 
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.query.Param;
@@ -12,6 +11,5 @@ import java.util.List;
 public interface Crm4PeriodicalReportRepository extends JpaRepository<Crm4PeriodicalReportModel, Integer> {
 
     @Procedure("Reporting_CDS4PeriodicalReport")
-    @Transactional
     List<Crm4PeriodicalReportModel> getReport(@Param("decisionRangeStart") String decisionRangeStart, @Param("decisionRangeEnd") String decisionRangeEnd);
 }
