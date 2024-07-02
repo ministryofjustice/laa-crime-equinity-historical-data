d604115e18000d2f507379b19d61796c2890175c
solomonmanik <solomon.manikyam@digital.justice.gov.uk>
2024-07-02 13:10:30 +0100

    EMP-321 savings section

---
+++ equinity-historical-data/src/main/java/uk/gov/justice/laa/crime/equinity/historicaldata/model/Crm15BankAccountDetailsModel.java
@@ -0,0 +1,26 @@
+package uk.gov.justice.laa.crime.equinity.historicaldata.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Crm15BankAccountDetailsModel {
+    @JsonProperty("Account_holder")
+    public String account_holder;
+    @JsonProperty("Bank")
+    public String bank;
+    @JsonProperty("Sort_code_brancg")
+    public String sort_code_brancg;
+    @JsonProperty("Account_type")
+    public String account_type;
+    @JsonProperty("Account_number")
+    public int account_number;
+    @JsonProperty("Overdrawn")
+    public boolean overdrawn;
+    @JsonProperty("Balance")
+    public double balance;
+}
