9e99a2cd71207e6e4d8da7bfcaf6d16555a12532
solomonmanik <solomon.manikyam@digital.justice.gov.uk>
2024-08-07 10:33:32 +0100

    EMP-424 code review comments

--- equinity-historical-data/src/main/java/uk/gov/justice/laa/crime/equinity/historicaldata/model/Crm14DetailsModel.java
+++ equinity-historical-data/src/main/java/uk/gov/justice/laa/crime/equinity/historicaldata/model/Crm14DetailsModel.java
@@ -820,8 +820,7 @@ public class Crm14DetailsModel extends Crm14AdditionalDetails implements CrmForm
     public int joint_properties_commercial;
     @JsonProperty("Joint_pieces_of_land")
     public int joint_pieces_of_land;
-    private boolean crm15;
-    public boolean isCrm15() {
+    public boolean hasCrm15() {
         if (StringUtils.isNotEmpty(this.getPrivate_company()) && this.getPrivate_company().equalsIgnoreCase("Yes"))
             return true;
         else return StringUtils.isNotEmpty(this.getPartner_private_company()) && this.getPartner_private_company().equalsIgnoreCase("Yes");
