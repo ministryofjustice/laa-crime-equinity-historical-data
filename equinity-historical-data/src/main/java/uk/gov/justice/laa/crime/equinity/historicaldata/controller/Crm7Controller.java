27b1772e02a2baa4d4c215c2b58c97615d94216a
Tomas <tomas.novellino@digital.justice.gov.uk>
2024-08-13 12:34:45 +0100

    EMP-182: Refactor to remove Sentry as a counter now that we can use micrometer for that
    EMP-182: Refactor to remove Sentry as a counter now that we can use micrometer for that

--- equinity-historical-data/src/main/java/uk/gov/justice/laa/crime/equinity/historicaldata/controller/Crm7Controller.java
+++ equinity-historical-data/src/main/java/uk/gov/justice/laa/crime/equinity/historicaldata/controller/Crm7Controller.java
@@ -1,8 +1,6 @@
 package uk.gov.justice.laa.crime.equinity.historicaldata.controller;
 
 import io.micrometer.core.annotation.Timed;
-import io.sentry.Sentry;
-import io.sentry.SentryLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -26,10 +24,7 @@ public class Crm7Controller implements Crm7InterfaceApi {
     @Override
     @Timed("laa_crime_equiniti_historic_data_view_crm7_details")
     public ResponseEntity<Crm7FormDTO> getApplicationCrm7(Long usn, String profileAcceptedTypes) {
-        String logMessage = String.format("eForm CRM7 details request received :: usn=[%s]", usn);
-        log.info(logMessage);
-        // TODO (EMP-182): This is only to count how many requests are received. Review to replace once other metric systems are introduced
-        Sentry.captureMessage(logMessage, SentryLevel.INFO);
+        log.info("eForm CRM7 details request received :: usn=[{}]", usn);
 
         CrmFormDetailsCriteriaDTO crmFormDetailsCriteriaDTO = new CrmFormDetailsCriteriaDTO(
                 usn, CRM_TYPE_7, profileAcceptedTypes
