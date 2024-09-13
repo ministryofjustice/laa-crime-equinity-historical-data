125179bfd9f1a8f837e2459c4f1b8f946436099b
solomonmanik <solomon.manikyam@digital.justice.gov.uk>
2024-09-13 15:49:53 +0100

    EMP-527 add processed attachments to evidence files

--- equinity-historical-data/src/main/java/uk/gov/justice/laa/crime/equinity/historicaldata/controller/Crm14Controller.java
+++ equinity-historical-data/src/main/java/uk/gov/justice/laa/crime/equinity/historicaldata/controller/Crm14Controller.java
@@ -11,6 +11,7 @@ import uk.gov.justice.laa.crime.equinity.historicaldata.generated.dto.Crm14FormD
 import uk.gov.justice.laa.crime.equinity.historicaldata.mapper.Crm14Mapper;
 import uk.gov.justice.laa.crime.equinity.historicaldata.model.Crm14AttachmentModel;
 import uk.gov.justice.laa.crime.equinity.historicaldata.model.Crm14Model;
+import uk.gov.justice.laa.crime.equinity.historicaldata.model.CrmEvidenceFileModel;
 import uk.gov.justice.laa.crime.equinity.historicaldata.service.Crm14AttachmentService;
 import uk.gov.justice.laa.crime.equinity.historicaldata.service.CrmFileService;
 
@@ -37,6 +38,26 @@ public class Crm14Controller implements Crm14InterfaceApi{
         Crm14Model crm14FormData = crmFileService.getCrmFormData(crmFormDetailsCriteriaDTO);
         List<Crm14AttachmentModel> attachments = crm14AttachmentService.getCrm14Attachments(usn);
         crm14FormData.getFormDetails().setProcessedAttachments(attachments);
+        addProcessedAttachmentsToEvidence(crm14FormData,attachments);
         return ResponseEntity.ok(mapper.getDTOFromModel(crm14FormData));
+    }
+
+    public void addProcessedAttachmentsToEvidence(Crm14Model crm14FormData, List<Crm14AttachmentModel> processedAttachments) {
+        /*if (CollectionUtils.isEmpty(processedAttachments)){
+            return;
+        }
+        /*
+        List<CrmEvidenceFileModel> files = crm14FormData.getEvidenceFiles().getFiles();
+        if (CollectionUtils.isEmpty(files)){
+            files = new ArrayList<>();
+        }
+        */
+        for(Crm14AttachmentModel processedAttachment: processedAttachments){
+            CrmEvidenceFileModel evidenceFileModel = new CrmEvidenceFileModel();
+            evidenceFileModel.setKey("att_"+processedAttachment.getAttachmentId()+".att");
+            evidenceFileModel.setName(processedAttachment.getFileName());
+            evidenceFileModel.setType(processedAttachment.getEvidenceType());
+            crm14FormData.getEvidenceFiles().getFiles().add(evidenceFileModel);
+        }
     }
 }
