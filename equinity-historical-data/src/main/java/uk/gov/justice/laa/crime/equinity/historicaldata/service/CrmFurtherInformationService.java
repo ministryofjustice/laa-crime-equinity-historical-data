ac7dc20a95c24bb901bdea01288dcfb3ad796b12
Tomas <tomas.novellino@digital.justice.gov.uk>
2024-10-02 10:54:39 +0100

    EMP-553: Fix unit test error and defelte deprecated version

--- equinity-historical-data/src/main/java/uk/gov/justice/laa/crime/equinity/historicaldata/service/CrmFurtherInformationService.java
+++ equinity-historical-data/src/main/java/uk/gov/justice/laa/crime/equinity/historicaldata/service/CrmFurtherInformationService.java
@@ -4,9 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import uk.gov.justice.laa.crime.equinity.historicaldata.model.*;
-import uk.gov.justice.laa.crime.equinity.historicaldata.model.crm4.Crm4Model;
-import uk.gov.justice.laa.crime.equinity.historicaldata.model.crm5.Crm5Model;
-import uk.gov.justice.laa.crime.equinity.historicaldata.model.crm7.Crm7Model;
 
 import java.util.List;
 
@@ -14,28 +11,6 @@ import java.util.List;
 @Service
 @Slf4j
 public class CrmFurtherInformationService {
-
-    public static final int CRM_TYPE_4 = 1;
-    public static final int CRM_TYPE_5 = 4;
-    public static final int CRM_TYPE_7 = 5;
-
-    public void addFileKeyToFurtherInfo(int crmType, CrmFormModelInterface crmModel) {
-        List<CrmFurtherInfoAttachmentsModel> furtherInfoAttachments = switch (crmType) {
-            case CRM_TYPE_4 -> ((Crm4Model) crmModel).getFormDetails().getFurtherInformationModel().getAttachments();
-            case CRM_TYPE_5 -> ((Crm5Model) crmModel).getFormDetails().getFurtherInformationModel().getAttachments();
-            case CRM_TYPE_7 -> ((Crm7Model) crmModel).getFormDetails().getFurtherInformationModel().getAttachments();
-            default -> null;
-        };
-
-        if (furtherInfoAttachments != null) {
-            for(CrmFurtherInfoAttachmentsModel furtherInfoAttachment: furtherInfoAttachments){
-                if(StringUtils.isNotEmpty(furtherInfoAttachment.getRetrieve()) && StringUtils.isNotEmpty(furtherInfoAttachment.getName())){
-                    furtherInfoAttachment.setFileKey(retrieveFileKey(furtherInfoAttachment.getName(),crmModel.getEvidenceFiles()));
-                }
-            }
-        }
-    }
-
     public void addFileKeyToFurtherInfo(CrmFormModelInterface crmModel) {
         List<CrmFurtherInfoAttachmentsModel> furtherInfoAttachments = ((CrmFurtherInformationModelInterface) crmModel.getFormDetails()).getFurtherInformationModel().getAttachments();
 
