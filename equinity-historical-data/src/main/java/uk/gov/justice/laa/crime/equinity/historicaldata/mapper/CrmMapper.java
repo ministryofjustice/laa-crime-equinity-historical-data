d6153a9034ed9f3a01c1a528fe2060aa41997a53
solomonmanik <solomon.manikyam@digital.justice.gov.uk>
2024-10-02 09:39:58 +0100

    EMP-521 sonar code dup fix

--- equinity-historical-data/src/main/java/uk/gov/justice/laa/crime/equinity/historicaldata/mapper/CrmMapper.java
+++ equinity-historical-data/src/main/java/uk/gov/justice/laa/crime/equinity/historicaldata/mapper/CrmMapper.java
@@ -1,8 +1,6 @@
 package uk.gov.justice.laa.crime.equinity.historicaldata.mapper;
 
-import org.apache.commons.lang3.StringUtils;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 import uk.gov.justice.laa.crime.equinity.historicaldata.generated.dto.CrmFurtherInformationDTO;
 import uk.gov.justice.laa.crime.equinity.historicaldata.model.CrmFurtherInfoAttachmentsModel;
 import uk.gov.justice.laa.crime.equinity.historicaldata.util.DateUtil;
@@ -10,12 +8,8 @@ import uk.gov.justice.laa.crime.equinity.historicaldata.util.DateUtil;
 import java.text.ParseException;
 import java.util.Date;
 
-import static uk.gov.justice.laa.crime.equinity.historicaldata.util.DateUtil.timeDifference;
-
 
 public interface CrmMapper {
-    static final String DEFAULT_START_DATETIME="1899-12-30T00:00:00";
-    static final String ISO_DATE_PATTERN="yyyy-MM-dd'T'HH:mm:ss";
     // Generic converters
     default Integer emptyIntToNull(String s) {
         return (s == null || s.isEmpty()) ? null : Integer.parseInt(s);
@@ -36,19 +30,6 @@ public interface CrmMapper {
     default <T extends Enum<T>> T convertToEnum(Class<T> enumClass, String s) {
         return (s == null || s.isEmpty()) ? null
             : Enum.valueOf(enumClass, s.toUpperCase().replace(" ", "_"));
-    }
-
-
-    @Named("convertToTimeSpentString")
-    default String convertToTimeSpentString(String t){
-        if (StringUtils.isEmpty(t)){
-            return null;
-        }
-        if (t.length() > 8){
-            return timeDifference(DEFAULT_START_DATETIME,t,ISO_DATE_PATTERN);
-        } else {
-            return t;
-        }
     }
     @Mapping(target = "name", source = "name")
     @Mapping(target="originalFileName", source = "originalfilename")
