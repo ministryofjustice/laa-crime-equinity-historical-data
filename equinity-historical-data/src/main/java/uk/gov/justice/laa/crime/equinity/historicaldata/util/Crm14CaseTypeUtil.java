8c439279a9250d3c73ee7c68149b027aa2ac9104
Tomas <tomas.novellino@digital.justice.gov.uk>
2024-10-17 14:54:26 +0100

    EMP-649: small refactoring to factor out caste type literals and into constants. Also simplifying converter function
    EMP-649: small refactoring to factor out caste type literals and into constants. Also simplifying converter function.
    EMP-649: fix null pointer exception by changing model attribute type to boolean, was Boolean. There is a function that will handle null cases

---
+++ equinity-historical-data/src/main/java/uk/gov/justice/laa/crime/equinity/historicaldata/util/Crm14CaseTypeUtil.java
@@ -0,0 +1,13 @@
+package uk.gov.justice.laa.crime.equinity.historicaldata.util;
+
+public class Crm14CaseTypeUtil {
+    public static String SUMMARY = "Summary-Only";
+    public static String INDICTABLE = "Indictable";
+    public static String EITHER_WAY = "Either-Way";
+    public static String TRIAL_CROWN_COURT = "Trial already in Crown Court";
+    public static String COMMITTAL = "Committal for Sentence";
+    public static String APPEAL_CROWN_COURT = "Appeal to Crown Court";
+    public static String APPEAL_NO_CHANGES = "Appeal to Crown Court and no changes";
+    public static String UNKNOWN = "Unknown";
+
+}
